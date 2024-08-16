a638248ea14d4003e4b4cda516e1ab3e797f1086
okayu <yuto.gio.0515@gmail.com>
2024-08-16 12:43:46 +0900

    3.3 Htmlヘルパーによるフォームの作成
    チェックボックス、ラジオボタン、ドロップダウンリスト、リストボックス

--- SampleRazorApp/Pages/Other.cshtml.cs
+++ SampleRazorApp/Pages/Other.cshtml.cs
@@ -3,13 +3,40 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
+
+    public enum Gender
+    {
+        male,
+        female
+    }
+
+    public enum Platform
+    {
+        Windows,
+        MacOS,
+        Linux,
+        ChromeOS,
+        Android,
+        iOS
+    }
+
     public class OtherModel : PageModel
     {
         public string Message { get; set; }
 
-        public void OnPost(string name, string password, string mail, string tel)
+        public bool check { get; set; }
+        public Gender gender { get; set; }
+        public Platform pc { get; set; }
+        public Platform[] pc2 { get; set; }
+
+        public void OnGet()
         {
-            Message = "Name : " + name + ", Password : " + password + ", Mail : " + mail + ", Tel : " + tel;
+            Message = "check & select it!";
+        }
+
+        public void OnPost(bool check, string gender, Platform pc, Platform[] pc2)
+        {
+            Message = $"ResultÅF{check}, {gender}, {pc}, {pc2.Length}";
         }
     }
 }
